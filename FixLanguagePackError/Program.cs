9ccbdfbad464e11e1feb96970046a35bcbdba827
Clemens <mail@clemens.jann.xyz>
2024-06-03 19:32:02 +0200

    feat: make .net6.0 compatible

--- FixLanguagePackError/Program.cs
+++ FixLanguagePackError/Program.cs
@@ -1,60 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Runtime.InteropServices;
-using FixLanguagePackError;
+﻿using System.Runtime.InteropServices;
 using Microsoft.Win32;
 
-if(!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
-    Console.WriteLine("Not run on windows");
-    Console.ReadLine();
-    return;
-}
-
-
-Console.WriteLine("Starting to write registry values");
-
-try
+namespace FixLanguagePackError
 {
-    Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate" , "DoNotConnectToWindowsUpdateInternetLocations", "0", RegistryValueKind.DWord);
-    Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate\\AU" , "UseWUServer", "0", RegistryValueKind.DWord);
-}
-catch (Exception e)
-{
-    Console.WriteLine("Failed to set registry values: " + e);
-    Console.ReadLine();
-    return;
-}
-
-Console.WriteLine("Installing language pack...");
-
-
-try
-{
-    int statusCode = PowerShell.ExecuteCommand("Install-Language -Language en-US");
-
-    if (statusCode != 0)
+    public class FixLanguagePackError
     {
-        Console.WriteLine("An error occurred. The installation was not successful.");
+        public static void Main(string[] args)
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Console.WriteLine("Not run on windows");
+                Console.ReadLine();
+                return;
+            }
+
+
+            Console.WriteLine("Starting to write registry values");
+
+            try
+            {
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate",
+                    "DoNotConnectToWindowsUpdateInternetLocations", "0", RegistryValueKind.DWord);
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate\\AU",
+                    "UseWUServer", "0", RegistryValueKind.DWord);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to set registry values: " + e);
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Installing language pack...");
+
+
+            try
+            {
+                int statusCode = PowerShell.ExecuteCommand("Install-Language -Language en-US");
+
+                if (statusCode != 0)
+                {
+                    Console.WriteLine("An error occurred. The installation was not successful.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            try
+            {
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate",
+                    "DoNotConnectToWindowsUpdateInternetLocations", "1", RegistryValueKind.DWord);
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate\\AU",
+                    "UseWUServer", "1", RegistryValueKind.DWord);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to set registry values: " + e);
+                Console.ReadLine();
+                return;
+            }
+
+
+            Console.ReadLine();
+
+
+        }
     }
 }
-catch (Exception e)
-{
-    Console.WriteLine(e);
-    throw;
-}
-
-try
-{
-    Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate" , "DoNotConnectToWindowsUpdateInternetLocations", "1", RegistryValueKind.DWord);
-    Registry.SetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows\\WindowsUpdate\\AU" , "UseWUServer", "1", RegistryValueKind.DWord);
-}
-catch (Exception e)
-{
-    Console.WriteLine("Failed to set registry values: " + e);
-    Console.ReadLine();
-    return;
-}
-
-
-Console.ReadLine();
-
